b78550cf9cf3ec668460d64b8c8a375b191de015
BenPerraud <benjaminperraud@outlook.fr>
2025-01-08 17:11:42 +0100

    v_seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,36 +4,18 @@ import Footer from "./components/Footer"
 import Header from './components/Header'
 
 export const metadata = {
-    title: 'TERRA OYA | Économisons l\'eau',
-    description: 'Fabrication française et vente d\'oyas d\'arrosage',
+    title: 'CARTE DE L\'EAU | L\'eau sous toutes ses formes',
+    description: 'S\'informer sur l\'eau en France de manière ludique et intéractive',
   }
 
 export default function RootLayout({children}) {
 
-  function schemaOrg () {
-    return {
-      __html: `{
-        "@context": "http://www.schema.org",
-        "@type": "ProfessionalService",
-        "name": "La Carte de l'Eau",
-        "url": "https://cartedeleau.fr",
-        "image": "",
-        "description": "La Carte de l'Eau est une application pour permettre aux citoyens de se renseigner sur la situation de l'eau en temps reèl en France",
-        "address": {
-          "@type": "PostalAddress",
-          "streetAddress": "13 Rue Principale",
-          "addressLocality": "CRISSEY",
-          "addressRegion": "Bourgogne",
-          "postalCode": "71530",
-          "addressCountry": "FRANCE"
-        },
-        "openingHours": "Mo, Tu, We, Th, Fr 9:00-18:00",
-        "contactPoint": {
-          "@type": "contactPoint",
-          "telephone": "+33 3 45 66 02 21"
-        }
-      }`
-    }
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'BlogPosting',
+    "headline": "LA CARTE DE L'EAU | L'eau sous toutes ses formes",
+    "name": "LA CARTE DE L'EAU | L'eau sous toutes ses formes",
+    "description" : "La Carte de l'Eau est une application pour permettre aux citoyens de se renseigner sur la situation de l'eau en temps réèl"
   }
   
     return (
@@ -42,9 +24,9 @@ export default function RootLayout({children}) {
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <title>LA CARTE DE L'EAU | Économisons l\'eau</title>
-          <meta name="title" content="LA CARTE DE L'EAU | Économisons l\'eau" />
-          <meta name="description" content="La Carte de l'Eau est une application pour permettre aux citoyens de se renseigner sur la situation de l'eau en temps reèl en France mise à disposition par l'entreprise TERRA OYA" />
-          <script type="application/ld+json" dangerouslySetInnerHTML={schemaOrg()} key="product-jsonld" />
+          <meta name="title" content="LA CARTE DE L'EAU | L'eau sous toutes ses formes" />
+          <meta name="description" content="La Carte de l'Eau est une application pour permettre aux citoyens de se renseigner sur la situation de l'eau en temps réèl en France" />
+          <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} key="product-jsonld" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" />
